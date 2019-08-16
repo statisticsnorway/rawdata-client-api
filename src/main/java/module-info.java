602b5a33b4af320c95fa6fe4f7f008bb1d179cd7
oranheim <oranheim@gmail.com>
2019-08-16 08:45:20 +0200

    Remove memory impl export in module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,7 +5,6 @@ module no.ssb.rawdata.api {
     requires transitive no.ssb.service.provider.api;
 
     exports no.ssb.rawdata.api;
-    exports no.ssb.rawdata.memory;
 
     provides RawdataClientInitializer with MemoryRawdataClientInitializer;
 }
