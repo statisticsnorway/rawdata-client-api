5d8d5bf913b7c506bd32ff0d83e0a0eb8a86376c
Kim Christian Gaarder <kim.christian.gaarder@gmail.com>
2021-03-01 09:34:03 +0100

    Allow interception of onComplete and onError signals

--- src/main/java/no/ssb/rawdata/api/RawdataFlows.java
+++ src/main/java/no/ssb/rawdata/api/RawdataFlows.java
@@ -1,6 +1,8 @@
 package no.ssb.rawdata.api;
 
 import java.util.concurrent.Flow;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 public class RawdataFlows {
@@ -11,5 +13,17 @@ public class RawdataFlows {
 
     public static Flow.Subscriber<RawdataMessage.Builder> subscriber(Supplier<RawdataProducer> producer) {
         return new RawdataSubscriber(producer);
+    }
+
+    public static Flow.Subscriber<RawdataMessage.Builder> subscriber(Supplier<RawdataProducer> producer, Consumer<RawdataProducer> onComplete) {
+        return new RawdataSubscriber(producer, onComplete);
+    }
+
+    public static Flow.Subscriber<RawdataMessage.Builder> subscriber(Supplier<RawdataProducer> producer, BiConsumer<Throwable, RawdataProducer> onError) {
+        return new RawdataSubscriber(producer, onError);
+    }
+
+    public static Flow.Subscriber<RawdataMessage.Builder> subscriber(Supplier<RawdataProducer> producer, Consumer<RawdataProducer> onComplete, BiConsumer<Throwable, RawdataProducer> onError) {
+        return new RawdataSubscriber(producer, onComplete, onError);
     }
 }
--- src/main/java/no/ssb/rawdata/api/RawdataSubscriber.java
+++ src/main/java/no/ssb/rawdata/api/RawdataSubscriber.java
@@ -4,6 +4,8 @@ import java.util.Objects;
 import java.util.concurrent.Flow;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 class RawdataSubscriber implements Flow.Subscriber<RawdataMessage.Builder> {
@@ -12,9 +14,31 @@ class RawdataSubscriber implements Flow.Subscriber<RawdataMessage.Builder> {
     final AtomicReference<RawdataProducer> producerRef = new AtomicReference<>();
     final AtomicReference<Flow.Subscription> subscriptionRef = new AtomicReference<>();
     final AtomicLong received = new AtomicLong();
+    final BiConsumer<Throwable, RawdataProducer> onError;
+    final Consumer<RawdataProducer> onComplete;
+
+    RawdataSubscriber(Supplier<RawdataProducer> producerSupplier, Consumer<RawdataProducer> onComplete, BiConsumer<Throwable, RawdataProducer> onError) {
+        this.producerSupplier = producerSupplier;
+        this.onError = onError;
+        this.onComplete = onComplete;
+    }
+
+    RawdataSubscriber(Supplier<RawdataProducer> producerSupplier, Consumer<RawdataProducer> onComplete) {
+        this.producerSupplier = producerSupplier;
+        this.onError = null;
+        this.onComplete = onComplete;
+    }
+
+    RawdataSubscriber(Supplier<RawdataProducer> producerSupplier, BiConsumer<Throwable, RawdataProducer> onError) {
+        this.producerSupplier = producerSupplier;
+        this.onError = onError;
+        this.onComplete = null;
+    }
 
     RawdataSubscriber(Supplier<RawdataProducer> producerSupplier) {
         this.producerSupplier = producerSupplier;
+        this.onError = null;
+        this.onComplete = null;
     }
 
     @Override
@@ -39,20 +63,28 @@ class RawdataSubscriber implements Flow.Subscriber<RawdataMessage.Builder> {
 
     @Override
     public void onError(Throwable throwable) {
-        Objects.requireNonNull(throwable);
-        subscriptionRef.set(null);
-        RawdataProducer producer = producerRef.getAndSet(null);
-        if (producer != null) {
-            try {
-                producer.close();
-            } catch (Exception e) {
-                e.printStackTrace();
+        try {
+            Objects.requireNonNull(throwable);
+            if (onError != null) {
+                onError.accept(throwable, producerRef.get());
             }
+        } finally {
+            close();
         }
     }
 
     @Override
     public void onComplete() {
+        try {
+            if (onComplete != null) {
+                onComplete.accept(producerRef.get());
+            }
+        } finally {
+            close();
+        }
+    }
+
+    private void close() {
         subscriptionRef.set(null);
         RawdataProducer producer = producerRef.getAndSet(null);
         if (producer != null) {
